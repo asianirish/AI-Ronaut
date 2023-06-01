0770dfd2766e20d88a01a1e0147d720cce604d6c
asianirish <asianirish@gmail.com>
2023-06-01 20:02:37 +0200

    modify MainWindow::onOpenNetworkConfig

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -83,7 +83,7 @@ void MainWindow::onOpenPlot()
 void MainWindow::onOpenNetworkConfig()
 {
     NetworkConfiWidget *wdt = new NetworkConfiWidget(this);
-//    wdt->updateCntx(page->cntx()); // TODO: updateClient
+    wdt->updateClient(&_client);
 
     int index = ui->tabWidget->addTab(wdt, tr("Network Config"));
     ui->tabWidget->setCurrentIndex(index);
