30d392036600e7a46a3d4e001803d582214b2c21
asianirish <asianirish@gmail.com>
2023-07-18 21:40:47 +0200

    delete extractJSON (global one)

--- oaic/Chat.cpp
+++ oaic/Chat.cpp
@@ -5,40 +5,6 @@
 #include <QJsonArray>
 #include <QJsonObject>
 
-// TODO: member static function?
-QStringList extractJSON(const QString& input) {
-    QStringList jsons;
-    int start = 0;
-    int end = 0;
-    int brackets = 0;
-    bool in_json = false;
-    bool in_string = false;
-    for (int i = 0; i < input.size(); ++i) {
-        QChar current = input[i];
-
-        if (current == '"' && (i == 0 || input[i - 1] != '\\')) {
-            in_string = !in_string;
-        }
-
-        if (!in_string) {
-            if (current == '{') {
-                if (!in_json) {
-                    start = i;
-                    in_json = true;
-                }
-                brackets++;
-            } else if (current == '}') {
-                brackets--;
-                if (brackets == 0 && in_json) {
-                    end = i;
-                    in_json = false;
-                    jsons.push_back(input.mid(start, end - start + 1));
-                }
-            }
-        }
-    }
-    return jsons;
-}
 
 namespace oaic {
 
