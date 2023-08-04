844e939cfa7485e3bddbccb4fbd25d13159ea12f
asianirish <asianirish@gmail.com>
2023-08-05 00:10:55 +0200

    edit copyrightInfo value

--- main.cpp
+++ main.cpp
@@ -114,7 +114,7 @@ int main(int argc, char *argv[])
     QCoreApplication::setApplicationName("ai-ronaut");
 
     QString copyrightInfo("/*\n"
- "* Copyright (c) 2022, Asianirish, asianirish@gmail.com"
+ "* Copyright (c) 2022, Asianirish, asianirish@gmail.com\n"
  "* \n"
  "* This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.\n"
  "* To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.\n"
