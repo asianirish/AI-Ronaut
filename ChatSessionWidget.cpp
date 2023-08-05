5d1ccd9df56f79351814327942d1cb6d785d8dad
asianirish <asianirish@gmail.com>
2023-08-05 11:34:40 +0200

    add copyright information

--- ChatSessionWidget.cpp
+++ ChatSessionWidget.cpp
@@ -1,3 +1,10 @@
+/*
+* Copyright (c) 2023, Asianirish, asianirish@gmail.com
+* 
+* This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+* To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+*/
+
 #include "ChatSessionWidget.h"
 #include "ui_ChatSessionWidget.h"
 
