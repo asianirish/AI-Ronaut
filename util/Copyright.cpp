783be2c20dcb0a1a82a33742bb6015e08e38e0f1
asianirish <asianirish@gmail.com>
2023-08-05 12:17:34 +0200

    delete a wrong copyright comment

--- util/Copyright.cpp
+++ util/Copyright.cpp
@@ -5,13 +5,6 @@
 * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 */
 
-/*
- * Copyright (c) 2022, Asianirish, asianirish@gmail.com
- *
- * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
- * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
- */
-
 #include "Copyright.h"
 
 const QStringList fileTypes { "*.h", "*.cpp" };
--- util/Copyright.h
+++ util/Copyright.h
@@ -5,12 +5,6 @@
 * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 */
 
-/*
- * Copyright (c) 2022, Asianirish, asianirish@gmail.com
- *
- * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
- * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
- */
 #ifndef COPYRIGHT_H
 #define COPYRIGHT_H
 
@@ -23,6 +17,12 @@
 class Copyright
 {
 public:
+    QString copyrightInfo("/*\n"
+                          "* Copyright (c) 2023, Asianirish, asianirish@gmail.com\n"
+                          "* \n"
+                          "* This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.\n"
+                          "* To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.\n"
+                          "*/\n");
     Copyright();
 
     static void addCopyright(const QString &path, const QString &copyrightInfo);
